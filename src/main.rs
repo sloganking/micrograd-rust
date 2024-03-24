0f20a013794d9b66d509f3ed44ce917bc2836a6c
sloganking <seanloganking@gmail.com>
2024-03-23 23:22:38 -0400

    🚧[main] use mlp

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
 mod graph;
+mod neuron;
 mod value;
 use crate::value::Value;
 
 fn main() {
-    let a = Value::from(3.0);
-    let b = Value::from(4.0);
-    let c = Value::from(5.0);
-    let d = (a - b) * c;
+    // let a = Value::from(3.0);
+    // let b = Value::from(4.0);
+    // let c = Value::from(5.0);
+    // let d = (a - b) * c;
 
-    d.backward();
+    // d.backward();
 
-    println!("{:#?}", *d);
+    // let neuron = neuron::Neuron::new(3);
+    // let inputs = vec![Value::from(1.0), Value::from(2.0), Value::from(3.0)];
+    // let out = neuron.forward(inputs);
+    // let out = out.tanh();
+    // out.backward();
 
-    if graph::render_graph(&d).is_none() {
+    // let layer = neuron::Layer::new(2, 3);
+    // let inputs = vec![Value::from(1.0), Value::from(2.0)];
+    // let out = layer.forward(inputs);
+
+    let mlp = neuron::MLP::new(3, vec![4, 4, 1]);
+
+    let inputs = vec![Value::from(1.0), Value::from(2.0), Value::from(3.0)];
+
+    let out = mlp.forward(inputs);
+
+    println!("{:?}", out);
+
+    let out = out[0].clone();
+
+    if graph::render_graph(&out).is_none() {
         println!("Error: Rendering dot file to an image failed. Please ensure that you have graphviz installed (https://graphviz.org/download/), and that the \"dot\" command is runnable from your terminal.");
     }
 }
